7285c1d2f40f035ba40a1d0023aec323ba94d594
Nataliee <nataliekc71@gmail.com>
2020-12-23 23:28:34 -0800

    Refactor to functional component

--- src/App.js
+++ src/App.js
@@ -1,68 +1,59 @@
+import React from 'react'
 import './App.css';
 import SelectRiskLevel from './components/select_risk_level';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ChartsContainer from './components/charts_container';
-import React from 'react'
 import { Button, ButtonGroup, Callout, Colors } from 'react-foundation';
 import logo from './logo.svg';
 import { Router, Redirect } from 'react-router';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-// const App = () => (
-//   <div>
-//     <BrowserRouter>
-//       <Switch>
-//         <Route path="/" component={SelectRiskLevel} />
-//         <Route path="/" component={ChartsContainer} />
-//       </Switch>
-//     </BrowserRouter>
-//   </div>
-// )
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      displayDonut: false
-    }
-    this.showDonutChart = this.showDonutChart.bind(this)
-  }
 
-  showDonutChart() {
-    this.setState({ displayDonut: true })
-  }
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={SelectRiskLevel} />
+      </Switch>
+    </BrowserRouter>
+  </div>
+)
 
-  render() {
-    if (this.state.displayDonut) {
-      return (
-        <div>
-          <ChartsContainer />
-        </div>
-      )
-    } else {
-      return (
-        <div>
-          <SelectRiskLevel />
-          <Button onClick={this.showDonutChart} color={Colors.ALERT} id='toDonutBt' >View in Donut Chart</Button>
-        </div>
-      )
-    }
-  }
-}
+export default App;
 
-// <div className="App">
-//   <header className="App-header">
-//     <img src={logo} className="App-logo" alt="logo" />
-//     <p>
-//       Edit <code>src/App.js</code> and save to reload.
-//     </p>
-//     <a
-//       className="App-link"
-//       href="https://reactjs.org"
-//       target="_blank"
-//       rel="noopener noreferrer"
-//     >
-//       Learn React
-//     </a>
-//   </header>
-// </div>
+//export default class App extends React.Component {
+// constructor(props) {
+//   super(props);
+//   this.state = {
+//     displayDonut: false
+//   }
+//   this.showDonutChart = this.showDonutChart.bind(this)
+// }
 
-//export default App;
+// showDonutChart() {
+//   this.setState({ displayDonut: true })
+// }
+
+//   render() {
+//     if (this.state.displayDonut) {
+//       return (
+//         <Router>
+//           <div>
+//             <Route path="/" component={ChartsContainer} />
+//           </div>
+//         </Router>
+//       )
+//     } else {
+//       return (
+//         <Router>
+//           <div>
+//             <Route path="/" component={SelectRiskLevel} />
+//             <Route />
+//             <Button onClick={this.showDonutChart} color={Colors.ALERT} id='toDonutBt'>View in Donut Chart</Button>
+//           </div>
+//         </Router>
+//       )
+//     }
+//   }
+// }
+
+
