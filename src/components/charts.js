55182871d041646531263f93af12bc402a931b63
Nataliee <nataliekc71@gmail.com>
2020-12-24 01:45:01 -0800

    Refactor to a class component

--- src/components/charts.js
+++ src/components/charts.js
@@ -1,62 +1,64 @@
 import React from 'react';
 import DonutChart from './donut_chart';
-import { maxCharts } from '../../src/data';
-import { Button, ButtonGroup, Callout, Colors } from 'react-foundation';
+import { Button, ButtonGroup } from 'react-foundation';
+import { Link, withRouter } from 'react-router-dom';
+import SelectRiskLevel from './select_risk_level'
 
-const Charts = ({ currentChart, onChangeChart }) => {
-    // class Charts extends React.Component {
-    //     constructor(props) {
-    //         super(props);
-    //     }
+class Charts extends React.Component {
 
-    // toNextPage(e){
-    //     this.props.history.push({
-    //         pathname: `/calculator`,
-    //         state: {
-    //             'riskLevel': ,
-    //             'Bonds': ,
-    //             'Large Cap': ,
-    //             'Mid Cap': ,
-    //             'Foreign': ,
-    //             'Small Cap' :
-    //         }
-    //     })
-    // }
+    constructor(props) {
+        super(props);
+        this.goBack = this.goBack.bind(this);
+        this.state = {
+            displayDonut: true
+        }
+    }
 
-    //render() {
-    return (
-        <div>
-            <div id='risk-selector-container'>
-                <div id='buttons-container'>
-                    <ButtonGroup onClick={(e) => {
-                        return (
-                            onChangeChart(e.target.innerText)
-                        )
-                    }}>
-                        <div id='risk-selector'>
-                            <ul class='risk-selector-ul'>
-                                <Button>1</Button>
-                                <Button>2</Button>
-                                <Button>3</Button>
-                                <Button>4</Button>
-                                <Button>5</Button>
-                                <Button>6</Button>
-                                <Button>7</Button>
-                                <Button>8</Button>
-                                <Button>9</Button>
-                                <Button >10</Button>
-                            </ul>
+    goBack() {
+        this.setState({ displayDonut: false })
+    }
+
+    render() {
+        if (!this.state.displayDonut) {
+            debugger
+            return (
+                <SelectRiskLevel />
+            )
+        } else {
+            return (
+                <div>
+                    <div id='risk-selector-container'>
+                        <div id='buttons-container'>
+                            <ButtonGroup onClick={(e) => {
+                                return (
+                                    this.props.onChangeChart(e.target.innerText)
+                                )
+                            }}>
+                                <div id='risk-selector'>
+                                    <ul class='risk-selector-ul'>
+                                        <Button>1</Button>
+                                        <Button>2</Button>
+                                        <Button>3</Button>
+                                        <Button>4</Button>
+                                        <Button>5</Button>
+                                        <Button>6</Button>
+                                        <Button>7</Button>
+                                        <Button>8</Button>
+                                        <Button>9</Button>
+                                        <Button >10</Button>
+                                    </ul>
+                                </div>
+                            </ButtonGroup>
+                            <Button id='continue' >Continue</Button>
+                            <Button id='back' onClick={this.goBack}>Back to Table</Button>
                         </div>
-                    </ButtonGroup>
-                    <Button id='continue' class='buttonC'>Continue</Button>
-                    <Button id='back'>Back to Table</Button>
-                </div>
-            </div>
-            <div id='donutChart'>
-                <DonutChart currentChart={currentChart - 1} />
-            </div>
-        </div>
-    );
+                    </div>
+                    <div id='donutChart'>
+                        <DonutChart currentChart={this.props.currentChart - 1} />
+                    </div>
+                </div >
+            );
+        }
+    }
 }
-
-export default Charts;
+export default withRouter(Charts);
