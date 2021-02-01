2f07885ee29b6a6af5bd2a92fb0631202263504a
Nataliee <nataliekc71@gmail.com>
2021-01-31 21:03:49 -0800

    Reorganize reducers into rootReducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,20 @@
 import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
+import cachePortfolioReducer from '../reducers/cache_portfolio_reducer';
 import changeDonutchartReducer from '../reducers/change_donutchart_reducer';
+import { combineReducers } from 'redux';
+import { logger } from 'redux-logger';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
+const rootReducer = combineReducers({
     changeDonutchartReducer,
-    composeEnhancer(applyMiddleware(thunk)),
+    cachePortfolioReducer,
+})
+
+const store = createStore(
+    rootReducer,
+    composeEnhancer(applyMiddleware(...[thunk, logger])),
 )
 
 export default store;
