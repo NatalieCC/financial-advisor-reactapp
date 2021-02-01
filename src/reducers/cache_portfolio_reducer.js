8f0caa475fa6fb87f7995189ace87ab1dbd7ae75
Nataliee <nataliekc71@gmail.com>
2021-01-31 20:36:40 -0800

    Add reducer for caching user input

---
+++ src/reducers/cache_portfolio_reducer.js
@@ -0,0 +1,15 @@
+//import actions TYPE
+import { cachePortfolio, RECEIVE_PORTFOLIO } from '../actions/cache_portfolio_actions';
+
+
+const cachePortfolioReducer = (state = {}, action) => {
+    switch (action.type) {
+        case RECEIVE_PORTFOLIO:
+            let { portfolio } = action;  //line 8 in action
+            return Object.assign({}, state, { portfolio });  //returning a copy of the old state merged with the currentchart from the action.
+        default:
+            return state;
+    }
+}
+
+export default cachePortfolioReducer;
